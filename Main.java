c820879ded407507b11b0246ae8e8c9335497c81
Jiza <Jiza>
2022-09-30 09:49:10 +0200

    mas codigo

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
     public static void main(String args[]){
         System.out.println("Que hay");
-        
+
+        int entrada=10
+
+        System.out.println(entrada);
     }
 }
